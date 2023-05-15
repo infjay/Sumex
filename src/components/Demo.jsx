d21530cead4b4468033e65e3d9a9fb3fed941d98
Jawid <41361157+infjay@users.noreply.github.com>
2023-05-15 18:46:31 +0200

    handleSubmit

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,10 +1,26 @@
 import { useState, useEffect } from "react"
 import { copy, linkIcon, loader, tick} from "../assets"
+import { useLazyGetSummaryQuery } from "../services/article"
+
+
 const Demo = () => {
-    const [article, setArticle] = useState({url:"",summary:""})
+    const [article, setArticle] = useState({
+        url:"",
+        summary:"",
+    })
     const handleSubmit = async (e) => {
-        alert("Submitted")
+        e.preventDefault()
+        const { data } = await getSummary({articleUrl: article.url})
+
+        if(data?.summary){
+            const newArticle = {...article,summary:data.summary }
+            setArticle(newArticle);
+            console.log(newArticle)
+        }
     }
+    const [getSummary, {error, isFetching }] = useLazyGetSummaryQuery();
+
+
   return (
     <section className="mt-16 w-full max-w-xl">
         <div className="flex flex-col w-full gap-2">
