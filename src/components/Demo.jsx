c7086ccfa1cbf27fae4dacc02d5c7a23a5d70437
Jawid <41361157+infjay@users.noreply.github.com>
2023-05-16 13:47:46 +0200

    Displaying the Article + Saving and Getting from LocalStorage + Update all articles

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -7,19 +7,42 @@ const Demo = () => {
     const [article, setArticle] = useState({
         url:"",
         summary:"",
-    })
+    });
+
+    const [allArticles, setAllArticles] = useState([]);
+    const [copied, setCopied] = useState("")
+
     const handleSubmit = async (e) => {
         e.preventDefault()
         const { data } = await getSummary({articleUrl: article.url})
 
         if(data?.summary){
             const newArticle = {...article,summary:data.summary }
+            const updatedAllArticles = [newArticle, ...allArticles];
+            
             setArticle(newArticle);
-            console.log(newArticle)
+            setAllArticles(updatedAllArticles);
+            // Updating LocalStorage
+            localStorage.setItem('articles',JSON.stringify(updatedAllArticles))
+            
         }
     }
+
     const [getSummary, {error, isFetching }] = useLazyGetSummaryQuery();
 
+    useEffect(()=> {
+        // Getting articles from Local Storage
+        const articlesFromLocalStorage = JSON.parse(localStorage.getItem("articles"))
+        if(articlesFromLocalStorage){
+            setAllArticles(articlesFromLocalStorage)
+        }
+    },[])
+
+    const handleCopy = (copyUrl) =>{
+        setCopied(copyUrl);
+        navigator.clipboard.writeText(copyUrl);
+        setTimeout(()=> setCopied(false),3000);
+    }
 
   return (
     <section className="mt-16 w-full max-w-xl">
@@ -41,6 +64,37 @@ const Demo = () => {
                     Summarize
                  </button>
             </form>
+            <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+                    {allArticles.map((item,index)=> (
+                    <div key={`link-${index}`}
+                    onClick={()=> setArticle(item)}
+                    className="link_card">
+                    <div className="copy_btn">
+                        <img src={copy} alt="copy icon" className="w-[40%] h-[40%] object-contain" />
+                    </div>
+                    <p className="flex-1 font-satoshi text-blue-700 text-sm truncate">{item.url}</p>
+                    </div>
+                    ))}
+            </div>
+        </div>
+        <div className="my-10 max-w-full flex justify-center items-center">
+                        {isFetching ? (<img src={loader} alt="loader" className="w-20 h-20 object-contain" />) : error ? (
+                            <p className="font-inter font-bold text-black text-center">An Error has occured! Please refresh the website and try again
+                            <br />
+                            <span className="font-satoshi font-normal text-gray-600">
+                                {error.data?.error}
+                            </span>
+                            </p>
+                        ) : (article.summary && (
+                            <div className="flex flex-col gap-3">
+                                <h2 className="font-satoshi font-bold text-gray-600 text-xl"> 
+                                    Article <span className="blue_gradient">Summary</span>
+                                </h2>
+                                <div className="summary_box">
+                                    <p className="font-inter font-medium text-sm text-gray-700">{article.summary} </p>
+                                </div>
+                            </div>
+                        )) }
         </div>
     </section>
   )
